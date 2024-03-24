81ae8be08dbfa2800a1d329144e6f721ccaf1628
Divya Paliwal <div.pali@gmail.com>
2024-03-24 16:24:07 +0530

    first commit

---
+++ src/main/java/com/example/NestedCommentsService/NestedCommentsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.NestedCommentsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestedCommentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestedCommentsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NestedCommentsService/NestedCommentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NestedCommentsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestedCommentsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
